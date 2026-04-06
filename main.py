1f086eb8df92ba6ab003afc250c06623b2fc7893
umangprajapati88-jpg <umangprajapati88@gmail.com>
2026-04-07 02:23:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,22 +18,17 @@ def home():
 
 import requests
 
+import requests
+
 @app.get("/market")
 def market():
-    session = requests.Session()
+    url = "https://api.allorigins.win/raw?url=https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY"
 
     headers = {
-        "User-Agent": "Mozilla/5.0",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Accept-Encoding": "gzip, deflate, br"
+        "User-Agent": "Mozilla/5.0"
     }
 
-    # Step 1: Get cookies
-    session.get("https://www.nseindia.com", headers=headers)
-
-    # Step 2: Fetch option chain
-    url = "https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY"
-    response = session.get(url, headers=headers)
+    response = requests.get(url, headers=headers)
 
     data = response.json()
 
